4c429fd5d2166f4971a705e151e825f9a3a6a36e
Jero Bado <tokidokitalkyou@gmail.com>
2020-04-10 20:08:05 +0800

    Added tests for parsing one or multiple commands
    Acceptable commands format are as follows:
    LEAF > till          # single
    LEAF > plant tomato  # multiple

---
+++ tests/test_leaf.py
@@ -0,0 +1,35 @@
+import unittest
+import sys
+
+
+class LeafTestCase(unittest.TestCase):
+
+    def test_single_command(self):
+        """
+        Test to accept only single command.
+
+        i.e. LEAF > till
+        """
+
+        command = input('LEAF (single) > ').split(maxsplit=1)
+        result = len(command)
+        expected = 1
+
+        self.assertEqual(expected, result)
+
+    def test_multiple_commands(self):
+        """
+        Test to accept multiple commands.
+
+        i.e. LEAF > plant tomato
+        """
+
+        command = input('LEAF (multiple) > ').split(maxsplit=1)
+        result = len(command)
+        expected = 2
+
+        self.assertEqual(expected, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
