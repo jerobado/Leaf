44dd1055bf52a59e50bc2eeffc638863fd2a68ae
Jero Bado <tokidokitalkyou@gmail.com>
2020-04-16 20:21:14 +0800

    Refactored gameloop.py to main.py
    Transferred the content of gameloop.py to mechanics.py

---
+++ src/main.py
@@ -0,0 +1,9 @@
+"""
+Leaf - simple text-based farming game for the bored developer.
+"""
+
+from src.mechanics import GameMechanics
+
+if __name__ == "__main__":
+    lets_play = GameMechanics()
+    lets_play.startgame()
