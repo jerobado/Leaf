57fe7cf22162a50dfda3ca006ad8a71d7fa22d45
Jero Bado <tokidokitalkyou@gmail.com>
2020-04-28 20:14:05 +0800

    Added test when the user enter no command

--- tests/test_leaf.py
+++ tests/test_leaf.py
@@ -12,18 +12,32 @@ class TestGameMechanics(unittest.TestCase):
 
     def test_GET_COMMANDS_function(self):
 
+        none_commands = ''
         single_commands = 'till'
         multiple_commands = 'plant lettuce'
 
         # See GameMechanics.get_commands() function for the full implementation
+        none_result = none_commands.split(maxsplit=1)
         single_result = single_commands.split(maxsplit=1)
         multiple_result = multiple_commands.split(maxsplit=1)
 
+        none_expected = []
         single_expected = ['till']
         multiple_expected = ['plant', 'lettuce']
 
+        self.assertEqual(none_expected, none_result)
         self.assertEqual(single_expected, single_result)
         self.assertEqual(multiple_expected, multiple_result)
+
+    def test_PARSE_COMMANDS_function_none(self):
+        """ Test parse_command() if with no given command by the player. """
+
+        # No commands
+        self.leafGameMechanics.rCommand = ''
+        self.leafGameMechanics.parse_commands()
+
+        self.assertIsNone(self.leafGameMechanics.isMultiple)
+        self.assertIsNone(self.leafGameMechanics.command)
 
     def test_PARSE_COMMANDS_function_single(self):
 
