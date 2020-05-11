b9d4b6407d638d81688ac71bda1d5c688802ca96
Jero Bado <tokidokitalkyou@gmail.com>
2020-05-12 06:08:01 +0800

    Created errors.py to hold custom game command errors.py
    List of errors
    - NoCommandError
    - MismatchCommandError
    - UnregisteredCommandError
    - IncompleteCommandError

---
+++ src/errors.py
@@ -0,0 +1,36 @@
+# Leaf Game Errors
+class LeafGameError(Exception):
+
+    pass
+
+
+class NoCommandError(LeafGameError):
+    """ Raised when the user enters a blank command. """
+
+    def __init__(self):
+
+        pass
+
+
+class MismatchCommandError(LeafGameError):
+    """ Raised when the user enter mismatch commands, i.e. till something. """
+
+    def __init__(self, command, argument):
+
+        print(f'\'{command}\' does not need to haxve an argument \'{argument}\'')
+
+
+class UnregisteredCommandError(LeafGameError):
+    """ Raised when the player enter an unregistered Leaf commands. i.e jump. """
+
+    def __init__(self, command):
+
+        print(f'\'{command}\' is not a valid command.\n\nSee \'help\' command.')
+
+
+class IncompleteCommandError(LeafGameError):
+    """ Raised when the player enters a command that requires an argument, i.e. plant or harvest. """
+
+    def __init__(self, command):
+
+        print(f'Incomplete \'{command}\', should be \'{command}\' [value], i.e. \'{command}\' value')
